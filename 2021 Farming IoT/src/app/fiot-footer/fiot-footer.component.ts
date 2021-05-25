340a9d1069c2629c6bba7206a5d918078c714346
e-popovic <popovic.ema59@gmail.com>
2021-05-25 02:36:04 +0200

    added farm IoT project

---
+++ 2021 Farming IoT/src/app/fiot-footer/fiot-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fiot-footer',
+  templateUrl: './fiot-footer.component.html',
+  styleUrls: ['./fiot-footer.component.css']
+})
+export class FiotFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
