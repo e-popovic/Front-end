4af3f19b334468b62bbb330833af16240ceb97e6
e-popovic <popovic.ema59@gmail.com>
2021-05-29 23:43:15 +0200

    Connected app to ThingSpeak sensors

--- 2021 Farming IoT/project/fiot-app/src/app/app.module.ts
+++ 2021 Farming IoT/project/fiot-app/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FiotHeaderComponent } from './fiot-header/fiot-header.component';
+import { FiotHeaderComponent } from './components/fiot-header/fiot-header.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
-import { FiotFooterComponent } from './fiot-footer/fiot-footer.component';
-import { FiotFrontPageComponent } from './fiot-front-page/fiot-front-page.component';
+import { FiotFooterComponent } from './components/fiot-footer/fiot-footer.component';
+import { FiotFrontPageComponent } from './components/fiot-front-page/fiot-front-page.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatCardModule} from "@angular/material/card";
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {MatCardModule} from "@angular/material/card";
     BrowserAnimationsModule,
     MatToolbarModule,
     MatExpansionModule,
-    MatCardModule
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
